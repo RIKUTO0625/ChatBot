f5886946525ce5f8a172de8ad6b6ec5f345d637f
Keiji1126 <knz2374475@stu.o-hara.ac.jp>
2024-11-11 11:55:19 +0900

    盛田　ログイン関連のjavaファイル追加

---
+++ src/login/DoctorLoginExecuteAction.java
@@ -0,0 +1,33 @@
+package login;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import tool.Action;
+
+public class DoctorLoginExecuteAction extends Action{
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		//ローカル変数の宣言 1
+		//なし
+		//リクエストパラメータ―の取得 2
+		//なし
+		//DBからデータ取得 3
+		//なし
+		//ビジネスロジック 4
+		//なし
+		//DBへデータ保存 5
+		//なし
+		//レスポンス値をセット 6
+		//なし
+		//JSPへフォワード 7
+
+//		エラーがあった場合ログイン画面へ戻る
+//		if(){
+//			req.getRequestDispatcher("doctor_login.jsp").forward(req, res);
+//		}
+
+		req.getRequestDispatcher("doctor_mypage.jsp").forward(req, res);
+	}
+}
