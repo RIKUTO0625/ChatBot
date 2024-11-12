1e2739c4fe1b03e7f3d3f7f74261aa74336ac2bd
Keiji1126 <knz2374475@stu.o-hara.ac.jp>
2024-11-12 09:53:44 +0900

    盛田　アカウント作成Javaファイル作成

---
+++ src/login/CreateAdminCompAction.java
@@ -0,0 +1,33 @@
+package login;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import tool.Action;
+
+public class CreateAdminCompAction extends Action{
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		//ローカル変数の宣言 1
+		//なし
+		//リクエストパラメータ―の取得 2
+		//なし
+		//DBからデータ取得 3
+		//なし
+		//ビジネスロジック 4
+		//なし
+		//DBへデータ保存 5
+		//なし
+		//レスポンス値をセット 6
+		//なし
+		//JSPへフォワード 7
+
+//		エラーがあった場合、プロフィール登録画面へ戻る
+//		if(){
+//			req.getRequestDispatcher("admin_create_pf.jsp").forward(req, res);
+//		}
+
+		req.getRequestDispatcher("admin_create_comp.jsp").forward(req, res);
+	}
+}
