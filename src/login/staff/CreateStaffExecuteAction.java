98b539e4e232828f693f55800b869438243f54f7
Tkm2004 <knz2374528@stu.o-hara.ac.jp>
2024-11-13 12:17:09 +0900

    CreateStaffCompAction 削除   CreateStaffExecuteAction 内容更新

--- src/login/staff/CreateStaffExecuteAction.java
+++ src/login/staff/CreateStaffExecuteAction.java
@@ -1,33 +1,47 @@
-package login.staff;
+package login;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import bean.Staff;
 import tool.Action;
 
-public class CreateStaffExecuteAction extends Action{
+public class CreateStaffExecuteAction extends Action {
 
-	@Override
-	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
-		//ローカル変数の宣言 1
-		//なし
-		//リクエストパラメータ―の取得 2
-		//なし
-		//DBからデータ取得 3
-		//なし
-		//ビジネスロジック 4
-		//なし
-		//DBへデータ保存 5
-		//なし
-		//レスポンス値をセット 6
-		//なし
-		//JSPへフォワード 7
+    @Override
+    public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+        // ローカル変数の宣言
+        HttpSession session = req.getSession(); // セッション
+        StaffDao sDao = new StaffDao(); // スタッフDaoを初期化
 
-//		エラーがあった場合、新規作成画面へ戻る
-//		if(){
-//			req.getRequestDispatcher("staff_create.jsp").forward(req, res);
-//		}
+        String ad_cd = req.getParameter("ad_cd"); // 組織コード
+        String staff_name = req.getParameter("staff_name"); // 職員名
+        String staff_mail = req.getParameter("staff_mail"); // 職員メールアドレス
+        String ad_pw = req.getParameter("ad_pw"); // パスワード
+        String staff_belong = req.getParameter("staff_belong"); // 所属
 
-		req.getRequestDispatcher("staff_create_pf.jsp").forward(req, res);
-	}
+        // DBへデータ保存
+        Staff staff = sDao.findByMail(staff_mail);
+        if (staff == null) { // 職員が未登録だった場合
+            // スタッフインスタンスを初期化
+            staff = new Staff();
+            // インスタンスに値をセット
+            staff.setAd_cd(ad_cd); // 組織コード
+            staff.setStaff_name(staff_name); // 職員名
+            staff.setStaff_mail(staff_mail); // 職員メールアドレス
+            staff.setAd_pw(ad_pw); // パスワード
+            staff.setAd_pw(staff_belong); // 所属
+
+            // 職員情報を保存
+            sDao.save(staff);
+        } else { // 入力されたメールアドレスがDBに保存されていた場合
+            req.setAttribute("error", "メールアドレスが重複しています");
+            req.getRequestDispatcher("staff_create.jsp").forward(req, res);
+            return;
+        }
+
+        // JSPへフォワード
+        req.getRequestDispatcher("staff_create_pf.jsp").forward(req, res);
+    }
 }
