d4d4099597df5f6e6a5c628aabbcf3261b40b8da
yuki <yamani.yuki8412@gmail.com>
2024-11-22 10:56:21 +0900

    新規メールアドレス作成関係の編集

--- src/login/admin/AddSendMailExecuteAction.java
+++ src/login/admin/AddSendMailExecuteAction.java
@@ -1,33 +1,46 @@
 package login.admin;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import bean.SendMail;
 import tool.Action;
 
 public class AddSendMailExecuteAction extends Action {
 
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
-		//ローカル変数の宣言 1
-		//なし
-		//リクエストパラメータ―の取得 2
-		//なし
-		//DBからデータ取得 3
-		//なし
-		//ビジネスロジック 4
-		//なし
-		//DBへデータ保存 5
-		//なし
-		//レスポンス値をセット 6
-		//なし
-		//JSPへフォワード 7
 
-//エラー発生時、作成画面から動かない
-//		if(){
-//			req.getRequestDispatcher("admin_mail_create.jsp").forward(req, res);
-//		}
+		//ローカル変数の宣言
+		HttpSession session = req.getSession();
+		SendMailDao smDao = new SendMailDao();
+
+		//リクエストパラメータ―の取得
+		String send_mail = req.getParameter("send_mail");
+		SendMail mail = null; //メールアドレス
+		Map<String, String> errors = new HashMap<>();// エラーメッセージ
+
+		//DBからデータ取得
+		mail = smDao.get(send_mail);
+
+		//新規のメールアドレス
+		if(mail == null){
+			mail = new SendMail(); //初期化
+			mail.setSend_mail(send_mail); //メールアドレス
+
+			//メールアドレス情報の登録
+			smDao.save(mail);
+
+		//既存のメールアドレス
+		} else {
+			errors.put("send_mail","そのメールアドレスは登録済みです");
+			return;
+		}
+
 		req.getRequestDispatcher("admin_mail_create_comp.jsp").forward(req, res);
 	}
-
 }
