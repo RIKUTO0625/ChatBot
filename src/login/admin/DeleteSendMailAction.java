b6aa530285f1723667d392b824dbbf16565c7abb
yuki <yamani.yuki8412@gmail.com>
2024-11-25 12:09:12 +0900

    微量の調整

--- src/login/admin/DeleteSendMailAction.java
+++ src/login/admin/DeleteSendMailAction.java
@@ -2,7 +2,9 @@ package login.admin;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import bean.SendMail;
 import tool.Action;
 
 public class DeleteSendMailAction  extends Action {
@@ -10,7 +12,18 @@ public class DeleteSendMailAction  extends Action {
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 
-		//JSPへフォワード 7
+		//ローカル変数の宣言
+		HttpSession session = req.getSession();
+		SendmailDao smDao = new SendmailDao();
+
+		//リクエストパラメータ―の取得
+		String send_mail = req.getParameter("send_mail");
+		SendMail mail = null; //メールアドレス
+
+		//DBからデータ取得
+		mail = smDao.get(send_mail);
+
+		//JSPへフォワード
 		req.getRequestDispatcher("admin_mail_delete.jsp").forward(req, res);
 	}
 
