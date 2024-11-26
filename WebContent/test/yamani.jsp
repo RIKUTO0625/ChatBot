a7aa8447acf2fa11279a3c9f1ead2f11a7bb2374
RIKUTO0625 <gojira1974612@gmail.com>
2024-11-26 10:56:09 +0900

    吉川：StaffDao

--- WebContent/test/yamani.jsp
+++ WebContent/test/yamani.jsp
@@ -19,6 +19,28 @@
 	<% out.println("★★★★"); %>
 
 	<h1>${staff.getStaff_id()}</h1>
+
+	<table class="table table-hover">
+		<tr>
+			<th></th>
+			<th>学生番号</th>
+			<th>氏名</th>
+			<th>クラス</th>
+			<th class="text-center">在学中</th>
+			<th></th>
+			<th></th>
+		</tr>
+		<c:forEach var="staff" items="${staff_list}">
+			<tr>
+				<td>${staff.getStaff_id()}</td>
+				<td>${staff.getStaff_name()}</td>
+				<td>${staff.getStaff_mail()}</td>
+				<td>${staff.getStaff_age()}</td>
+				<td class="text-center">s
+			</tr>
+		</c:forEach>
+	</table>
+
 	<h1>${admin.getAd_cd()}</h1>
 	<h1>${doctor.getIs_deleted()}</h1>
 
--- src/test/YamaniAction.java
+++ src/test/YamaniAction.java
@@ -1,5 +1,7 @@
 package test;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -19,17 +21,43 @@ public class YamaniAction extends Action{
 
 		rds test = new rds();
 
+        //職員ログイン：〇
 		StaffDao staff_dao = new StaffDao();
 		Staff staff = staff_dao.loginStaff("staff000", "pass");
 
 
+		//職員アカウント作成：〇
+//		Staff created_staff = new Staff();
+//		Admin created_admin = new Admin();
+//		created_admin.setAd_cd("000000");
+//
+//		created_staff.setStaff_id("staff002");
+//		created_staff.setStaff_name("削除用");
+//		created_staff.setStaff_belong("総務課");
+//		created_staff.setStaff_pw("pass");
+//		created_staff.setStaff_mail("example@gmail.com");
+//		created_staff.setAdmin(created_admin);
+//		staff_dao.createStaff(created_staff);
+
+        //プロフィール編集：〇
+		staff.setFavorite("read book");
+		staff.setStaff_mail("example@gmail.com");
+		staff_dao.editStaff(staff);
+
+		//職員一覧表示：
+		List<Staff> staff_list = staff_dao.viewStaff(staff);
+
+		//組織ログイン：〇
 		AdminDao admin_dao = new AdminDao();
 	    Admin admin = admin_dao.loginAdmin("000000", "pass");
 
+	    //医者ログイン：〇
 	    DoctorDao doctor_dao = new DoctorDao();
 	    Admin doctor = doctor_dao.loginDoctor("pass", "000000");
 
+
 	    req.setAttribute("staff", staff);
+	    req.setAttribute("staff_list", staff_list);
 	    req.setAttribute("admin", admin);
 	    req.setAttribute("doctor", doctor);
 
