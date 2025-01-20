35eb7fd1f9f9b0ffa977717a88b7af0af149b2ab
Keiji1126 <knz2374475@stu.o-hara.ac.jp>
2025-01-20 14:09:13 +0900

    チャットボットJavaとりあえず完成

--- src/login/staff/UserChatBotAction.java
+++ src/login/staff/UserChatBotAction.java
@@ -25,47 +25,74 @@ public class UserChatBotAction extends Action{
         Map<String, String> errors = new HashMap<>(); // エラーメッセージ
         ChatDao cDao = new ChatDao();
 
-        List<Chat>list = new ArrayList<>();
+        List<Chat>list = new ArrayList<>();		//履歴のリスト
+        List<String>question_list = new ArrayList<>();	//質問の履歴リスト
+        List<String>answer_list = new ArrayList<>();	//回答の履歴リスト
+        String question_new;	//次の質問
 
         // セッションから"staff"という名前でStaffオブジェクトを取得
         Staff staff = (Staff) session.getAttribute("user");
 
 		//リクエストパラメータ―の取得 2
-        Integer question = req.getParameter("question");		//質問
-        Integer answer = req.getParameter("answer");		//解答
+
+        String question_st = req.getParameter("question");	//質問
+        String answer_st = req.getParameter("answer");		//回答
 
 		//DBからデータ取得 3
-        list = cDao.getChat(staff, 1, 1);
-        System.out.println(list);
-
 
 		//ビジネスロジック 4
 
         //質問と回答内容の記録
-        if (answer != null && question != null){
+        if (answer_st != null && question_st != null){
+            Integer question = Integer.parseInt(question_st);	//Int型に変換
+            Integer answer = Integer.parseInt(answer_st);		//Int型に変換
         	cDao.setChat(staff, answer, question);
         }
 
-        // 質問だけを抽出
-        List<Integer> questionlist = list.stream()
-                                   	.map(Chat::getQue_no) // getQue_noメソッドを使用して質問を取得
-                                   	.collect(Collectors.toList()); // リストに収集
 
-        // 解答だけを抽出
-        List<Integer> answerlist = list.stream()
-                                   	.map(Chat::getAns_no) // getAns_noメソッドを使用して解答を取得
-                                   	.collect(Collectors.toList()); // リストに収集
+        //チャット履歴の取得
+        list = cDao.getChat(staff, 1, 1);
+        System.out.println(list);
 
-        System.out.println(questionlist);
-        System.out.println(answerlist);
 
+        if(list != null){	//履歴の格納
+	        // que_noだけを抽出
+	        List<Integer> que_no_list = list.stream()
+	                                   	.map(Chat::getQue_no) // getQue_noメソッドを使用して質問を取得
+	                                   	.collect(Collectors.toList()); // リストに収集
+
+	        // ans_noだけを抽出
+	        List<Integer> ans_no_list = list.stream()
+	                                   	.map(Chat::getAns_no) // getAns_noメソッドを使用して解答を取得
+	                                   	.collect(Collectors.toList()); // リストに収集
+
+	        System.out.println(que_no_list);
+	        System.out.println(ans_no_list);
+
+	        //質問内容のリスト化
+	        for (Integer data : que_no_list){
+	        	question_list.add(cDao.getQueText(data));
+	        }
+
+	        //回答内容のリスト化
+	        for (Integer data : ans_no_list){
+	        	answer_list.add(cDao.getAnsText(data));
+	        }
+
+	        question_new = cDao.getQueText(list.size()+1);
+
+        }
+        else {	//初回だった場合
+        	question_new = cDao.getQueText(1);
+
+        }
 		//DBへデータ保存 5
 		//なし
 
 		//レスポンス値をセット 6
-        req.setAttribute("question_list", questionlist);	//今までの質問
-        req.setAttribute("answer_list", answerlist);		//今までの解答
-        req.setAttribute("question", question);		//次に渡される質問
+        req.setAttribute("question_list", question_list);	//今までの質問
+        req.setAttribute("answer_list", answer_list);		//今までの回答
+        req.setAttribute("question", question_new);		//次に渡される質問
 
 		//JSPへフォワード 7
 		req.getRequestDispatcher("chatbot.jsp").forward(req, res);
