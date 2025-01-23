fe1032539852d3b49a4e6238665bf543476a5609
Keiji1126 <knz2374475@stu.o-hara.ac.jp>
2025-01-23 11:14:27 +0900

    履歴Java修正

--- src/login/doctor/UserLogAction.java
+++ src/login/doctor/UserLogAction.java
@@ -21,22 +21,44 @@ public class UserLogAction extends Action{
 		HttpSession session = req.getSession();//セッション
 		StaffDao sDao = new StaffDao();
 		ChatDao cDao = new ChatDao();
-		Integer year;
+		Integer year;	//年
+		Staff staff;	//職員情報
 		List<List<Integer>>staff_log = new ArrayList<>();	//質問の履歴リスト
 
 		//リクエストパラメータ―の取得 2
+		String years = req.getParameter("year");
+		String month = req.getParameter("month");
 		String staff_id = req.getParameter("no");
 		String staff_pw = req.getParameter("pw");
 
 		//DBからデータ取得 3
-		Staff staff = sDao.loginStaff(staff_id, staff_pw);
 
-		year = Year.now().getValue();
+		if(staff_id != null && staff_pw != null){
+			staff = sDao.loginStaff(staff_id, staff_pw);
+			session.setAttribute("staff", staff);
+		}
+		else{
+			staff = (Staff)session.getAttribute("staff");// 職員情報を取得
+		}
+
+		if(years != null){
+			year = Integer.parseInt(years);
+		}
+		else {
+			year = Year.now().getValue();
+		}
 
 		staff_log = cDao.getHis(staff, year);
 		System.out.println(staff_log);
 
-
+		//質問ごとの回答の統計を渡す処理
+        // カウントを1からスタートする
+        int index = 1;
+        for (List<Integer> sublist : staff_log) {
+            String key = "qu_list" + index; // キー名を qu_list1, qu_list2, ... とする
+            req.setAttribute(key, sublist); // サーブレットのリクエストオブジェクトに設定
+            index++;
+        }
 
 
 		//ビジネスロジック 4
@@ -45,7 +67,7 @@ public class UserLogAction extends Action{
 		//なし
 		//レスポンス値をセット 6
 		req.setAttribute("staffLog", staff_log);
-        req.setAttribute("staff", staff);
+        req.setAttribute("name", staff.getStaff_name());
 		//JSPへフォワード 7
 		req.getRequestDispatcher("user_log.jsp").forward(req, res);
 	}
