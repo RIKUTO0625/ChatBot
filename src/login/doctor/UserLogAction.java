ead3381ce8db63fd2725105f8253700e4bf5dd43
Keiji1126 <knz2374475@stu.o-hara.ac.jp>
2025-01-28 14:10:55 +0900

    修正

--- src/login/doctor/UserLogAction.java
+++ src/login/doctor/UserLogAction.java
@@ -52,11 +52,11 @@ public class UserLogAction extends Action{
 			year = Year.now().getValue();
 		}
 
-		if(months != "null"){	//月をINT型に変更
+		if(months != "0"){	//月をINT型に変更
 			month = Integer.parseInt(months);
 		}
 
-		if(months == "null"){
+		if(months == "0"){
 			staff_log = cDao.getHis(staff, year);
 		}
 		else{
